510ee048ec483daa819b54688ef63556c092d26c
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2025-05-03 10:59:58 +0000

    Enhance logging script to include GitHub username

--- log_time.sh
+++ log_time.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "Current Date and Time: $(date)" >> log.txt
+
+# Get the currently authenticated GitHub username
+GITHUB_USER=$(gh api user --jq .login)
+
+# Log the current date, time, and GitHub username
+echo "[$(date)] - GitHub User: $GITHUB_USER" >> log.txt
+
+
